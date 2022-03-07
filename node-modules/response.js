01c8756a8d4b9d52d9c1f763a313383febffdd76
Mason <100867480+SusanoSix@users.noreply.github.com>
2022-03-07 12:24:26 -0500

    Re-upload of first Udemy module utility practice.
    Added files to folder for cleaner look and optimized navigation

---
+++ node-modules/response.js
@@ -0,0 +1,11 @@
+function decrypt(data) {
+    return 'decrypted data';
+}
+
+function read() {
+    return decrypt('data')
+}
+
+module.exports = {            //module.exports = | export { 
+    read,
+};
